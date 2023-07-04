297dc87a76157b5fff00585ed0b7b8f53885414c
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-07-05 08:10:58 +0900

    Introducing Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:deeplink/app.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
