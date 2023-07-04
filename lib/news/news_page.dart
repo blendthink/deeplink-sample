879af6d6b0c822959c572ba321ffdc524425d40a
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-07-05 07:56:26 +0900

    Create MainPage, HomePage, NewsPage

---
+++ lib/news/news_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NewsPage extends StatelessWidget {
+  const NewsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
