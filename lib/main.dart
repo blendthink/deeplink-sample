588800d2ee7a725724cc1bee75a65b75c6094034
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-07-05 07:54:08 +0900

    Move the app to another file

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
+import 'package:deeplink/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
 }
